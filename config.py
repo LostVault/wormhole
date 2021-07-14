b9b695338f0942ae00465532bd9a26acaa220660
beigbeider <beigbeider@gmail.com>
2021-07-14 18:11:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 ## ОСНОВНЫЕ
 
 # Токен для авторизации приложения:
-token = 'ODY0ODQ1NTIxNzA5NTYzOTE1.YO7YKA.QugT2vy9wV0-oX6FWX3vsHc6IC8'
+token = 'ODI2NDEwODk1NjM0MzMzNzE4.YGMFHw.aIamZ2OGVVD0td7uPVOEue13Sns'
 
 # Преффикс комманд:
 prefix = '!'
 
 # Название глобального канал:
-globalchannel = 'ctd'
+globalchannel = 'grid'
 
 ## ОСНОВЫНЕ // КОНЕЦ
