1f17f60c9a05bdf84b322bd95b6352596bb8f6b1
beigbeider <beigbeider@gmail.com>
2021-07-15 00:31:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,19 @@
 
 # import os
 # import sys
+# import json
 import discord                                      # Импортируем основной модуль
 from discord.ext import commands                    # Импортируем команды из модуля discord.ext
 from discord.ext.commands import has_permissions
-import asyncio                                      # Испортируем модуль для работы с асинхронными командами
+# import asyncio                                      # Испортируем модуль для работы с асинхронными командами
 import logging                                      # Импортируем моудль логирования
 # import random                                     # Импортируем модуль генерирования случайных чисел
 # import datetime                                   # Импортируем модуль времени
 
 import config                                       # Импортируем настройки приложения
 # ------------- ИМПОРТ МОДУЛЕЙ // КОНЕЦ
+
+blackoverlordlist = ["665018860587450388", "777300091621998632"]
 
 
 # Создаём приложение и называем его client
@@ -30,9 +33,14 @@ async def on_ready():
     print(' Client ID: {0.user.id} '.format(client))  # Показывает ID приложения указанное на discordapp.com
     print(' Link for connection: https://discordapp.com/oauth2/authorize?&client_id={0.user.id}&scope=bot&permissions=0'.format(client))
     print(' Hello world!')
+    print('-••••••••••••••••••••••••••••••-')
+    print('Servers connected to:')
+    for guild in client.guilds:
+        print(guild.name)
     print('-••••••••••••••••••••••••••••••-\n')
+
     # Изменяем статус приложения
-    await client.change_presence(status=discord.Status.online,activity=discord.Game('Elite Dangerous'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Elite Dangerous'))
 # ------------- ВЫВОДИМ ДАННЫЕ ПРИЛОЖЕНИЯ ПРИ ПОДКЛЮЧЕНИЕ В КОНСОЛЬ // КОНЕЦ
 
 
@@ -48,24 +56,41 @@ async def on_message(message):
 
     channel = discord.utils.get(message.guild.text_channels, name=config.globalchannel)
 
-    # Игнорируем сообщения отправленные приложением
-    if message.author.id == client.user.id:
-        return
     # Игнорируем сообщения отправленные на других каналах
     if message.channel.id != channel.id:
         return
+
     # Игнорируем сообщения с упоминанием
     if message.mentions or message.mention_everyone:
-        await message.delete()
-        await channel.send('`` ⚠ • ВНИМАНИЕ! `` Сообщения с упоминанием всех активных и неактивных пользователей не пропускаются в общий чат.'.format(message), delete_after=13)
+        # --
+        try:
+            await message.delete()
+        except:
+            pass
+        # --
+        await channel.send('` ⚠ • ВНИМАНИЕ! ` Сообщения с упоминанием всех активных и неактивных пользователей не пропускаются в общий чат.'.format(message), delete_after=13)
         return
-    # Игнорируем сообщения с символом @
+
+    # Игнорируем сообщения отправленные приложением
+    if message.author.id == client.user.id:
+        return
+
+    #    if message.author.id == blackoverlordlist:
+    #        await channel.send('` ⚠ • ВНИМАНИЕ! ` Пользователи нахоядщиеся в списке Black Overlord List не могут отправлять собщения на другие сервера.'.format(message), delete_after=13)
+    #        return
+
+        # Игнорируем сообщения с символом @
     if "@" in message.content:
         await message.delete()
-        await channel.send('`` ⚠ • ВНИМАНИЕ! `` Упс! Что-то пошло не так.'.format(message), delete_after=13)
+        await channel.send('` ⚠ • ВНИМАНИЕ! ` Упс! Что-то пошло не так.'.format(message), delete_after=13)
         return
+
     # Удаляем сообщение отправленное пользователем
-    await message.delete()
+    try:
+        await message.delete()
+    except:
+        pass
+
     for guild in client.guilds:
         if channel := discord.utils.get(guild.text_channels, name=config.globalchannel):
             try:
@@ -101,38 +126,57 @@ async def on_command_error(ctx, error, amount=1):
 
 # ------------- КОММАНДА ПРОВЕРКА ПРИЛОЖЕНИЯ
 @client.command(aliases=['пинг'], brief='Проверка состояния приложения', pass_context=True)
+# Команду может выполнить только владельце приложения
+@commands.is_owner()
 async def ping(ctx, amount=1):
     # Удаляем сообщение отправленное пользователем
     await ctx.channel.purge(limit=amount)
     # Отправляем сообщение и удаляем его через 13 секунд
-    await ctx.send(f'{ctx.author.mention} pong! ({client.latency*1000}ms)', delete_after=13)
+    await ctx.send(f'` **{ctx.author.name}** ` Pong! ({client.latency*1000}ms)', delete_after=13)
 # ------------- КОММАНДА ПРОВЕРКА ПРИЛОЖЕНИЯ // КОНЕЦ
 
 
-# ------------- УДАЛЕНИЕ СООБЩЕНИЙ НА КАНАЛЕ
+# ------------- КОМАНДА ВЫВОДА СПИСКА СЕРВЕРОВ
+@client.command(aliases=['сервера'], brief='Проверка состояния приложения', pass_context=True)
+# Команду может выполнить только владельце приложения
+@commands.is_owner()
+async def servers(ctx, amount=1):
+    activeservers = client.guilds
+    for guild in activeservers:
+        await ctx.send(guild.name)
+        print(guild.name)
+# ------------- КОМАНДА ВЫВОДА СПИСКА СЕРВЕРОВ // КОНЕЦ
+
+
+# ------------- КОМАНДА УДАЛЕНИЯ СООБЩЕНИЙ НА КАНАЛЕ
 @client.command(aliases=['очистить'], brief='Удаление ста последних сообщений на канале', pass_context=True)
 # Разрешаем выполнение команды только пользователям с ролью администратор
 @has_permissions(administrator=True)
 async def clear(ctx, amount=100):
     # Удаляем сто последних сообщений на канале
     await ctx.channel.purge(limit=amount)
-# ------------- УДАЛЕНИЕ СООБЩЕНИЙ НА КАНАЛЕ // КОНЕЦ
+# ------------- КОМАНДА УДАЛЕНИЯ СООБЩЕНИЙ НА КАНАЛЕ // КОНЕЦ
 
 
 # ------------- ОТКЛЮЧЕНИЕ ПРИЛОЖЕНИЯ ПО КОМАНДЕ
 @client.command(aliases=['выключить'], brief='Выключение приложения по команде', pass_context=True)
-# Разрешаем выполнение команды только пользователям с ролью администратор
-@has_permissions(administrator=True)
+# Команду может выполнить только владельце приложения
+@commands.is_owner()
 async def shutdown(ctx, amount=1):
     # Удаляем сообщение отправленное пользователем
-    await ctx.channel.purge(limit=amount)
+    try:
+        await ctx.channel.purge(limit=amount)
+    except:
+        pass
     # Отправляем сообщение и удаляем его через 13 секунд
-    await ctx.send(f'За что {ctx.author.mention}, мы же так хорошо общались!', delete_after=13)
+    for guild in client.guilds:
+        if channel := discord.utils.get(guild.text_channels, name=config.globalchannel):
+            await channel.send('` ⚠ • ВНИМАНИЕ! ` Приложение отключено.')
     # Отключаем приложение
-    await ctx.bot.logout()
     print('\n-••••••••••••••••••••••••••••••-')
     print(' Goodbye World!')
     print('-••••••••••••••••••••••••••••••-\n')
+    quit()
 # ------------- ОТКЛЮЧЕНИЕ ПРИЛОЖЕНИЯ ПО КОМАНДЕ// КОНЕЦ
 
 
