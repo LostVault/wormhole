205fe28d6cf40f99aa88668c282db077c89f492d
beigbeider <beigbeider@gmail.com>
2021-07-15 19:18:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,9 @@
 # Токен для авторизации приложения:
 token = 'ODI2NDEwODk1NjM0MzMzNzE4.YGMFHw.aIamZ2OGVVD0td7uPVOEue13Sns'
 
+# Токен для авторизации тестового приложения:
+# token = 'ODY0ODQ1NTIxNzA5NTYzOTE1.YO7YKA.QugT2vy9wV0-oX6FWX3vsHc6IC8'
+
 # Преффикс комманд:
 prefix = '!'
 
