6087bb3b16014f3ff53dab2507c69dea94d0777c
beigbeider <beigbeider@gmail.com>
2021-07-15 22:04:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ async def on_message(message):
         await channel.send('` ⚠ • ВНИМАНИЕ! ` Пользователи нахоядщиеся в списке **Black Overlord List** не могут отправлять собщения на другие сервера.'.format(message), delete_after=13)
         return
 
-        # Игнорируем сообщения с символом @
+    # Игнорируем сообщения с символом @
     if "@" in message.content:
         await message.delete()
         await channel.send('` ⚠ • ВНИМАНИЕ! ` Упс! Что-то пошло не так.'.format(message), delete_after=13)
@@ -101,10 +101,12 @@ async def on_message(message):
     for guild in client.guilds:
         if channel := discord.utils.get(guild.text_channels, name=config.globalchannel):
             try:
-                # Создаём приложение
+                # Создаём сообщение
                 emGlobalMessage = discord.Embed(description=''+ message.author.mention +' — '+ message.content +'', colour=discord.Colour(16711684))
                 emGlobalMessage.set_footer(icon_url=message.guild.icon_url, text=message.guild.name)
+                # Отправляем сообщение
                 await channel.send(embed=emGlobalMessage)
+                # Отправляем сообщение
                 # await channel.send(' ` {0.guild.name} ` — **` {0.author.name} `**: {0.content}'.format(message))
             except discord.Forbidden:
                 print(f"System: Невозможно отправить сообщение на сервер {guild.name}: Недостаточно прав")
@@ -195,18 +197,17 @@ async def shutdown(ctx, amount=1):
 # Команду может выполнить только владельце приложения
 @commands.is_owner()
 async def information(ctx, amount=1):
-    activeservers = client.guilds
-    for guild in activeservers:
+    # Удаляем сообщение отправленное пользователем
+    await ctx.channel.purge(limit=amount)
+    for guild in client.guilds:
         # Создаём сообщение
         emInformation = discord.Embed(title='Информация', description='Приложение создано для передачи текстовых сообщений между серверами связанными с игрой *Elite Dangerous*.', colour=discord.Colour(16711684))
         emInformation.add_field(name='Разработчики ', value='• <@420130693696323585>\n• <@665018860587450388>')
         emInformation.add_field(name='Благодарности', value='• <@478527700710195203>')
-        #eminformation.add_field(name='Список серверов', value='' + guild.name + '')
+        emInformation.add_field(name='Список серверов', value='' + guild.name + '')
         emInformation.set_footer(text=' '+ client.user.name +' ')
         # Отправляем сообщение и удаляем его через 13 секунд
         await ctx.send(embed=emInformation, delete_after=60)
-
-        # await ctx.send(guild.name)
 # ------------- КОМАНДА ОТОБРАЖЕНИЯ ИФОРМАЦИИ О ПРИЛОЖЕНИЕ // КОНЕЦ
 
 
